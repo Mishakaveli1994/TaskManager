cc89ac534277cf2ee8f951ed7e1691a0967bf979
Mishakaveli1994 <mihata1994@yahoo.com>
2022-04-27 21:35:07 +0300

    Added Allowed Hosts

--- TaskManager/settings.py
+++ TaskManager/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = utilities.APP_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://task-manager-softuni.herokuapp.com/','localhost', '127.0.0.1']
 
 # Application definition
 
