6cb526e180fd0a2720dccdf57930ba9e54615370
Mishakaveli1994 <mihata1994@yahoo.com>
2022-04-30 01:08:44 +0300

    Debug Mode set to false

--- TaskManager/settings.py
+++ TaskManager/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = utilities.APP_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://task-manager-softuni.herokuapp.com/',
                  'localhost',
